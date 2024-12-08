aff2620d6a4fa11858233ab68f8730e167c95759
KimHyunJin <khj745700@naver.com>
2024-12-09 04:21:41 +0900

    be/feat: hashtag 도메인 추가
    #10

---
+++ backend/src/main/java/shop/khj745700/backend/domain/hashtag/domain/HashTag.java
@@ -0,0 +1,29 @@
+package shop.khj745700.backend.domain.hashtag.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "hashtag")
+@Entity
+public class HashTag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "hashtag_id")
+    private Integer id;
+
+    @Column(name = "hashtag_name")
+    private String name;
+
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<HashTagList> hashTagList;
+
+}
---
+++ backend/src/main/java/shop/khj745700/backend/domain/hashtag/domain/HashTagRepository.java
@@ -0,0 +1,6 @@
+package shop.khj745700.backend.domain.hashtag.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HashTagRepository extends JpaRepository<HashTag, Integer> {
+}
