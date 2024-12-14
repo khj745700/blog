0925964ce632a498dd493fce0ee203187f34344a
KimHyunJin <khj745700@naver.com>
2024-12-15 08:04:03 +0900

    fe/feat: 이미지 업로드 및 조회 기능 추가
    #13

--- frontend/src/pages/BoardEditView.vue
+++ frontend/src/pages/BoardEditView.vue
@@ -8,10 +8,14 @@ import {computed, ref} from 'vue';
 import InputBox from "@/atomic/InputBox.vue";
 import RippleButton from "@/atomic/RippleButton.vue";
 import HashTagListComponent from "@/components/HashTagListComponent.vue";
+import imageUploadRequest from "@/api/image/ImageUpload.js";
 
 const handleUpload = async (file) => {
   console.log(file)
-  return 'https://i.postimg.cc/52qCzTVw/pngwing-com.png'
+  let path;
+  await imageUploadRequest(file, res => path = res.headers['location'], err => console.error(err));
+  console.log(path);
+  return path;
 }
 
 const hashtags = ref([
@@ -31,6 +35,31 @@ const hashtagComputed = computed(() => {
   return hashtags.value.map(hashtag => hashtag.name);
 })
 
+async function getFileName(files) {
+  console.log(files);
+  const fileName = files[0];
+  imageUploadRequest(fileName, (res)=>console.log(res.headers['location']), err => console.error(err));
+  await uploadFile(fileName);
+
+}
+
+function uploadFile(file) {
+  // 비동기적으로 동작하기 위하여 promise를 return 해준다.
+  return new Promise(resolve => {
+    // 업로드된 파일을 읽기 위한 FileReader() 객체 생성
+    let a = new FileReader()
+    // 읽기 동작이 성공적으로 완료됐을 때 발생
+    a.onload = e => {
+      resolve(e.target.result)
+      // 썸네일을 보여주고자 하는 <img>에 id값을 가져와 src에 결과값을 넣어준다.
+      const previewImage = document.getElementById('preview')
+      previewImage.src = e.target.result
+    }
+    // file 데이터를 base64로 인코딩한 문자열. 이 문자열을 브라우저가 인식하여 원래 데이터로 만들어준다.
+    a.readAsDataURL(file)
+  })
+}
+
 </script>
 
 
@@ -38,12 +67,12 @@ const hashtagComputed = computed(() => {
   <PageContainer>
     <div class="titleContainer">
       <div class="titleBody">
-        <InputBox  input-type="text" placeholder="제목을 입력하세요"></InputBox>
+        <InputBox  input-type="text" placeholder="제목을 입력하세요" ></InputBox>
       </div>
       <div style="display: flex; justify-content: center; align-items: center">
-        <input type="file" id="upload-thumbnail" hidden/>
+        <input type="file" id="upload-thumbnail" hidden @change="getFileName($event.target.files)"/>
         <label for="upload-thumbnail">
-          <img src="@/assets/camera.png" />
+          <img src="@/assets/camera.png" id="preview"/>
         </label>
       </div>
     </div>
@@ -69,11 +98,15 @@ const hashtagComputed = computed(() => {
 
 
 <style scoped>
-  .titleBody {
 
+  #preview {
+    width: 124px;
+    height: 124px;
+  }
+
+  .titleBody {
     border: 1px solid black;
     width: 70%;
-
   }
 
   .titleContainer {
