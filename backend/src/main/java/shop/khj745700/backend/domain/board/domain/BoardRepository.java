4926ad18bfe6b1c4548c95bfd52c098cf8c578bf
KimHyunJin <khj745700@naver.com>
2024-12-15 23:47:15 +0900

    be/fix: 게시글 페이지네이션 조건문 수정
    #15

--- backend/src/main/java/shop/khj745700/backend/domain/board/domain/BoardRepository.java
+++ backend/src/main/java/shop/khj745700/backend/domain/board/domain/BoardRepository.java
@@ -11,6 +11,6 @@ public interface BoardRepository extends JpaRepository<Board, Integer> {
     @Query("SELECT new shop.khj745700.backend.domain.board.rest.dto.BoardView(b) FROM Board b WHERE b.wroteDate IS NOT NULL AND b.isHidden = false ORDER BY b.wroteDate DESC ")
     Slice<BoardView> findAllBoards(Pageable pageable);
 
-    @Query("SELECT new shop.khj745700.backend.domain.board.rest.dto.BoardView(b) FROM HashTag ht LEFT JOIN HashTagList htl ON ht = htl.hashTag LEFT JOIN Board b ON htl.board = b WHERE ht.id = :id AND b.isHidden = false AND b.wroteDate != null ORDER BY b.wroteDate DESC ")
+    @Query("SELECT new shop.khj745700.backend.domain.board.rest.dto.BoardView(b) FROM HashTag ht LEFT JOIN HashTagList htl ON ht = htl.hashTag LEFT JOIN Board b ON htl.board = b WHERE ht.id = :id AND b.isHidden = false AND b.wroteDate IS NOT NULL ORDER BY b.wroteDate DESC ")
     Slice<BoardView> findAllBoardsByHashTagId(Integer id, Pageable pageable);
 }
